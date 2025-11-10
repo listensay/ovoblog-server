4eb87ba94737ddfe0d14e83b0629e2b815d634a7
codemmk <2107228359@qq.com>
2025-11-10 21:21:49 +0800

    feat: 代码修复

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
 import { PostModule } from './modules/post/post.module';
-import { ImagesModule } from './modules/image/images.module';
 import { TagModule } from './modules/tag/tag.module';
-import { LinksModule } from './modules/link/links.module';
-import { CommentsModule } from './modules/comments/comments.module';
 import { ConfigModule } from './modules/config/config.module';
 import { User } from './modules/user/entities/user.entity';
 import { Category } from './modules/category/entities/category.entity';
 import { Post } from './modules/post/entities/post.entity';
-import { Images } from './modules/image/entities/image.entity';
 import { Tags } from './modules/tag/entities/tag.entity';
-import { Links } from './modules/link/entities/link.entity';
-import { Comments } from './modules/comments/entities/comment.entity';
 import { Config } from './modules/config/entities/config.entity';
-import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -27,19 +20,15 @@ import { UsersModule } from './modules/users/users.module';
       username: 'root',
       password: '123456',
       database: 'ovoblog',
-      entities: [User, Category, Post, Images, Tags, Links, Comments, Config],
+      entities: [User, Category, Post, Tags, Config],
       synchronize: true,
       timezone: '+08:00',
     }),
     UserModule,
     CategoryModule,
-    PostModule,
-    ImagesModule,
     TagModule,
-    LinksModule,
-    CommentsModule,
+    PostModule,
     ConfigModule,
-    UsersModule,
   ],
 })
 export class AppModule {}
