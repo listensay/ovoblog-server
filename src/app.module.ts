bede67e816001e090e957bb0199ecf31287fbdf0
codemmk <2107228359@qq.com>
2025-11-10 15:16:33 +0800

    feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
+import { CategoryModule } from './modules/category/category.module';
+import { PostModule } from './modules/post/post.module';
+import { ImagesModule } from './modules/images/images.module';
+import { TagModule } from './modules/tag/tag.module';
+import { LinksModule } from './modules/links/links.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { ConfigModule } from './modules/config/config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'ovoblog',
+      entities: [],
+      synchronize: true,
+      timezone: '+08:00',
+    }),
+    UserModule,
+    CategoryModule,
+    PostModule,
+    ImagesModule,
+    TagModule,
+    LinksModule,
+    CommentsModule,
+    ConfigModule,
+  ],
 })
 export class AppModule {}
